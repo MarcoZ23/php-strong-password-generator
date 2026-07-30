3a05373d94b946b58847dfebe3e3e4beb7cd0935
Marco Zollo <zollomarco23@gmail.com>
2026-07-30 19:56:03 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php 
+   function generator($lunghezza) {
+    
+    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?%&@#*+-_$';
+    
+   $password = ''; 
+
+   for ($i = 0; $i < $lunghezza; $i++) {
+        $posizione = rand(0, strlen($caratteri) -1);
+        $password .= $caratteri[$posizione];
+   } 
+   return $password;
+   }
+  
+   // var_dump($_GET) ; 
+
+   if (isset($_GET['lunghezza'])) {
+        echo generator($_GET['lunghezza']);
+    } 
+
+   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Password generator</title>
+</head>
+<body>
+
+    <div class="container bg-dark mb-3">
+        <h1 class="text-center text-secondary">Strong Password Generator</h1>
+        <h3 class="text-center text-white mt-3">Genera una password sicura</h3> 
+        <div class="container bg-white text-dark">
+            <form class="row justify-content-between p-3" action="index.php" method="get">
+                <label class="col-form-label col-3">Lunghezza password:</label>
+                <div class="col-3">
+                    <input class="form-control border border-primary" type="number" name="lunghezza">
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Genera</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
