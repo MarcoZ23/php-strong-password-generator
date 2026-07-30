4919277ff3a24f61330aaf71ee49ff609ddb5fa0
Marco Zollo <zollomarco23@gmail.com>
2026-07-30 20:05:15 +0200

    Move password generator function to functions.php

--- index.php
+++ index.php
@@ -1,16 +1,5 @@
 <?php 
-   function generator($lunghezza) {
-    
-    $caratteri = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!?%&@#*+-_$';
-    
-   $password = ''; 
-
-   for ($i = 0; $i < $lunghezza; $i++) {
-        $posizione = rand(0, strlen($caratteri) -1);
-        $password .= $caratteri[$posizione];
-   } 
-   return $password;
-   }
+require_once 'functions.php';
   
    // var_dump($_GET) ; 
 
